caca15917e332d1f0b594e17458ee1157548711e
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2020-05-25 17:48:07 -0400

    :dizzy: Hooks - Setting State with Hooks
    Hooks - Setting State with Hooks

---
+++ complete-intro-to-react-v5-0f7977ae3ce923ecf7094b82056a809fcaf1e905/src/SearchParams.js
@@ -0,0 +1,25 @@
+import React, { useState }  from 'react';
+
+const  SearchParams = () => {
+    const [location, setLocation] = useState("Seattle, WA")
+
+    return(
+        <div className="search-params"> 
+            <form>
+                <label>
+                    Location
+                    <input id="location" value={location}
+                    placeholder="Location"
+                    onChange={event => setLocation(event.target.value)} />
+                </label>
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default SearchParams
+
+//putting the input inside of a label is good for accessability so that if someone clicks on the location that it highlights the correct input. 
+
+//location is the current state of location, setLocation is the updated state.
