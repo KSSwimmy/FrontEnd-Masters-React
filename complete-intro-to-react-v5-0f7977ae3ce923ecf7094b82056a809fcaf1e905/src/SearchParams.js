473d29905d9a2a4d91763a06f3ab11cac0488fd5
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2020-05-26 23:09:29 -0400

    :twisted_rightwards_arrows: Async & Routing - Asynchronous API Requests and Using the Fallback Mock API
    Async & Routing - Asynchronous API Requests and Using the Fallback Mock API

--- complete-intro-to-react-v5-0f7977ae3ce923ecf7094b82056a809fcaf1e905/src/SearchParams.js
+++ complete-intro-to-react-v5-0f7977ae3ce923ecf7094b82056a809fcaf1e905/src/SearchParams.js
@@ -7,14 +7,25 @@ const SearchParams = () => {
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+
+    setPets(animals || []);
+  }
 
   useEffect(() => {
     setBreeds([]);
     setBreed("");
 
-    pet.breeds(animal).then(({ breeds }) => {
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
       //const breedStrings = breeds.map(({ breedObj }) => breedObj.name);same thing as above
-      const breedStrings = breeds.map(({ name }) => name);
+      const breedStrings = apiBreeds.map(({ name }) => name);
       setBreeds(breedStrings);
     }, console.error);
   }, [animal, setBreed, setBreeds]);
@@ -22,7 +33,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form
+    onSubmit={e => {
+    e.preventDefault();
+    requestPets();
+  }}
+>
         <label>
           Location
           <input
