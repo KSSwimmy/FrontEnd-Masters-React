249f359f93ce656cc4991a20ac11cfe498289bf1
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2020-05-27 23:33:13 -0400

    :busts_in_silhouette: Reach Router
    Reach Router

--- Async-Routing/src/App.js
+++ Async-Routing/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router } from "@reach/router";
+import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>;
     </div>
   );
 };
